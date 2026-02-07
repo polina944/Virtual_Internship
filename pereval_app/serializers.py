3afefac1f2d4cf92786d92a0cc68519027382f08
Polina <zernina.polina@gmail.com>
2026-02-07 18:32:30 +0300

    Добавили в PerevalSerializer метод create для устранения дублирования пользователей при создании новых перевалов

--- pereval_app/serializers.py
+++ pereval_app/serializers.py
@@ -42,3 +42,20 @@ class PerevalSerializer(WritableNestedModelSerializer):
             'add_time', 'user', 'coords', 'level', 'status', 'images'
         ]
 
+    def create(self, validated_data):
+        user_data = validated_data.pop('user')
+        coords_data = validated_data.pop('coords')
+        level_data = validated_data.pop('level')
+        images_data = validated_data.pop('images')
+
+        user, _ = User.objects.get_or_create(**user_data)
+        coords = Coords.objects.create(**coords_data)
+        level = Level.objects.create(**level_data)
+
+        pereval = Pereval.objects.create(user=user, coords=coords, level=level, **validated_data)
+
+        for image in images_data:
+            Images.objects.create(pereval=pereval, **image)
+
+        return pereval
+
