f4bd1b2bcab8c4000798f2e983e8fa61156dc478
Polina <zernina.polina@gmail.com>
2026-02-07 18:17:29 +0300

    Добавили вьюсеты по моделям

--- pereval_app/views.py
+++ pereval_app/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from pereval_app.models import Pereval, User, Coords, Level, Images
+from pereval_app.serializers import PerevalSerializer
+
+
+class UserViewSet(ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = PerevalSerializer
+
+
+class CoordsViewSet(ModelViewSet):
+    queryset = Coords.objects.all()
+    serializer_class = PerevalSerializer
+
+
+class LevelViewSet(ModelViewSet):
+    queryset = Level.objects.all()
+    serializer_class = PerevalSerializer
+
+
+class ImagesViewSet(ModelViewSet):
+    queryset = Images.objects.all()
+    serializer_class = PerevalSerializer
+
+
+class PerevalViewSet(ModelViewSet):
+    queryset = Pereval.objects.all()
+    serializer_class = PerevalSerializer
+
