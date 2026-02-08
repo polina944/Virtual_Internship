f37913c8e7c2ea7ea89fa9cabd1bea6ee70f2327
Polina <zernina.polina@gmail.com>
2026-02-08 10:15:37 +0300

    Добавили фильтрацию перевалов по email-пользователя

--- pereval_app/views.py
+++ pereval_app/views.py
@@ -32,6 +32,7 @@ class PerevalViewSet(ModelViewSet):
     queryset = Pereval.objects.all()
     serializer_class = PerevalSerializer
     http_method_names = ['get', 'post', 'patch']
+    filterset_fields = ['user__email']
 
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
