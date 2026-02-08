20a174cbbf1663e12219f9f464a9b82bf4a912d1
Polina <zernina.polina@gmail.com>
2026-02-08 10:40:58 +0300

    Добавили тест для проверки получения списка перевалов

--- pereval_app/tests.py
+++ pereval_app/tests.py
@@ -1,3 +1,91 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+from pereval_app.models import Pereval, User, Coords, Level, Images
+from pereval_app.serializers import PerevalSerializer
+
+
+class PerevalTest(APITestCase):
+    def setUp(self):
+        self.pereval_1 = Pereval.objects.create(
+            beauty_title='beauty_title_1',
+            title='title_1',
+            other_titles='other_titles_1',
+            connect='',
+            add_time='2021-09-22T13:18:13Z',
+            user=User.objects.create(
+                email='email_1@email.com',
+                fam='fam_1',
+                name='name_1',
+                otc='otc_1',
+                phone='89000000001',
+            ),
+            coords=Coords.objects.create(
+                latitude=123.00,
+                longitude=456.00,
+                height=789
+            ),
+            level=Level.objects.create(
+                winter='1A',
+                summer='1B',
+                autumn='2B',
+                spring='',
+            )
+        )
+
+        self.image_1_1 = Images.objects.create(
+            data='data_1_1',
+            title='title_1_1',
+            pereval=self.pereval_1,
+        )
+        self.image_1_2 = Images.objects.create(
+            data='data_1_2',
+            title='title_1_2',
+            pereval=self.pereval_1,
+        )
+
+        self.pereval_2 = Pereval.objects.create(
+            beauty_title='beauty_title_2',
+            title='title_2',
+            other_titles='other_titles_2',
+            connect='',
+            add_time='2021-09-22T13:18:13Z',
+            user=User.objects.create(
+                email='email_2@email.com',
+                fam='fam_2',
+                name='name_2',
+                otc='otc_2',
+                phone='89000000002',
+            ),
+            coords=Coords.objects.create(
+                latitude=123.02,
+                longitude=456.02,
+                height=792
+            ),
+            level=Level.objects.create(
+                winter='1A',
+                summer='1B',
+                autumn='',
+                spring='2B',
+            )
+        )
+
+        self.image_2_1 = Images.objects.create(
+            data='data_2_1',
+            title='title_2_1',
+            pereval=self.pereval_2,
+        )
+        self.image_2_2 = Images.objects.create(
+            data='data_2_2',
+            title='title_2_2',
+            pereval=self.pereval_2,
+        )
+
+    def test_get_list(self):
+        url = reverse('pereval-list')
+        response = self.client.get(url)
+        serializer_data = PerevalSerializer([self.pereval_1, self.pereval_2], many=True).data
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data, serializer_data)
+        self.assertEqual(2, len(serializer_data))
